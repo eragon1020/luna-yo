2580c08b570c89cbe25d643336d93f110fbbb809
angel luna <angelstivengarcialuna@gmail.com>
2024-06-20 19:01:18 -0500

    termine

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -1,48 +1,44 @@
 <template>
-  <div class="2xl:w-60" name="container">
+  <div class="2xl:w-60 " name="container">
     <div class="static ">
-        <h1 class="text-[#fff] text-4xl absolute  top-24 left-24">Welcome Back</h1>
-        <p class="text-[#ffffff] text-xl  absolute top-32 left-28">Nice to see you again!</p>
-        <img
-        src="../assets/futuristic-technology-background-blue-line-wave-light-screen-abstract-illustration-photo.jpg"
-        alt="Logo"
-        class="mx-auto my-4 rounded-md"
-      />
+        <h1 class="text-[#fff] text-4xl absolute  top-24 left-24 sm:text-7xl sm:top-52 sm:left-40">Welcome Back</h1>
+        <p class="text-[#ffffff] text-xl  absolute top-32 left-28 sm:text-4xl sm:top-72 sm:left-48">Nice to see you again!</p>
+ 
       </div>
-    <div class="bg-[#f1f1f1]">
-      <div class="bg-[#fff]">
-        <div class="">
-          <img src="../assets/image.jpg" class="m-auto" />
+    <div class="sm:ml-96 sm:w-8/12 sm:rounded-md sm:h-full">
+      <div class="bg-[#fff]  sm:ml-96 sm:w-8/12 sm:rounded-3xl sm:h-full">
+        <div class=" ">
+          <img src="../assets/image.jpg" class="m-auto sm:mr-48" />
 
-          <h1 class="my-10 text-4xl text-center">Login</h1>
+          <h1 class="my-10 text-4xl text-center sm:mr-20">Login</h1>
         </div>
         <form name="login-form" class="">
-          <div class="ml-12">
+          <div class="ml-12 sm:ml-24">
             <label for="username" class="">Username</label>
             <div class="relative">
               <input
                 type="text"
                 id="username"
                 v-model="input.username"
-                class="p-2 rounded-lg m-auto my-4 w-80 shadow-lg shadow-[#cecece]"
+                class="p-2 rounded-lg m-auto my-4 w-80 shadow-lg shadow-[#a9d2fa]"
               />
               <i class=""></i>
             </div>
           </div>
-          <div class="ml-12">
+          <div class="ml-12 sm:ml-24">
             <label for="password" class="">Password</label>
             <div class="relative">
               <input
                 :type="showPassword ? 'text' : 'password'"
                 id="password"
                 v-model="input.password"
-                class="p-2 rounded-lg m-auto my-4 w-80 shadow-lg shadow-[#cecece]"
+                class="p-2 rounded-lg m-auto my-4 w-80 shadow-lg shadow-[#a9d2fa]"
               />
               <i @click="togglePasswordVisibility"></i>
             </div>
             <a href="#" class="">Forgot password?</a>
           </div>
-          <div class=" ml-8">
+          <div class=" ml-8 sm:ml-24">
             <input
               type="checkbox"
               v-model="condiciones"
@@ -50,12 +46,12 @@
               name="condiciones"
               class=""
             />
-            <label for="condiciones" class="ml-3"
-              >I acknowledge and agree to comply with the stated policies and
+            <label for="condiciones" class="ml-3" 
+              >I acknowledge and agree to comply with <br/>  thestated policies and 
               terms</label
             >
           </div>
-          <div class="ml-8">
+          <div class="ml-8 sm:ml-24">
             <input type="checkbox" name="remember" class="mr-3" />
             <label for="remember" class="mr-2">Remember me</label>
           </div>
@@ -63,14 +59,12 @@
             type="submit"
             @click.prevent="login"
             :disabled="isButtonDisabled"
-            class="bg-[#127dd6] w-96 h-10 rounded-xl ml-5 mt-5"
+            class="bg-[#127dd6] w-96 h-10 rounded-xl ml-5 mt-5 cursor-pointer sm:mb-12 sm:ml-24 sm:w-80"
           >
             Start
           </button>
         </form>
       </div>
-    
-    
     </div>
   </div>
 </template>
