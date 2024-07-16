c712b4afde9a7979a9ef04c881897d44f1b7e37b
eragon1020 <brayan.aragon.zabala@gmail.com>
2024-07-16 09:19:56 -0500

    ultimo

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -1,68 +1,44 @@
 <template>
-  <div class="2xl:w-60 " name="container">
-    <div class="static ">
-        <h1 class="text-[#fff] text-4xl absolute  top-24 left-24 sm:text-7xl sm:top-52 sm:left-40">Welcome Back</h1>
-        <p class="text-[#ffffff] text-xl  absolute top-32 left-28 sm:text-4xl sm:top-72 sm:left-48">Nice to see you again!</p>
- 
-      </div>
+  <div class="2xl:w-60 flex flex-col items-center" name="container">
+    <div class="relative w-full">
+      <h1 class="text-[#fff] text-4xl absolute top-24 left-24 sm:text-7xl sm:top-52 sm:left-40">Welcome Back</h1>
+      <p class="text-[#ffffff] text-xl absolute top-32 left-28 sm:text-4xl sm:top-72 sm:left-48">Nice to see you again!</p>
+    </div>
     <div class="sm:ml-96 sm:w-8/12 sm:rounded-md sm:h-full">
-      <div class="bg-[#fff]  sm:ml-96 sm:w-8/12 sm:rounded-3xl sm:h-full">
-        <div class=" ">
+      <div class="bg-[#fff] sm:ml-96 sm:w-8/12 sm:rounded-3xl sm:h-full p-8">
+        <div class="text-center sm:text-left">
           <img src="../assets/image.jpg" class="m-auto sm:mr-48" />
-
-          <h1 class="my-10 text-4xl text-center sm:mr-20">Login</h1>
+          <h1 class="my-10 text-4xl">Login</h1>
         </div>
-        <form name="login-form" class="">
-          <div class="ml-12 sm:ml-24">
-            <label for="username" class="">Username</label>
+        <form name="login-form" class="flex flex-col items-center">
+          <div class="w-full sm:w-8/12">
+            <label for="username" class="block ml-12 sm:ml-0">Username</label>
             <div class="relative">
-              <input
-                type="text"
-                id="username"
-                v-model="input.username"
-                class="p-2 rounded-lg m-auto my-4 w-80 shadow-lg shadow-[#a9d2fa]"
-              />
+              <input type="text" id="username" v-model="input.username" class="p-2 rounded-lg my-4 w-full shadow-lg shadow-[#a9d2fa]" />
               <i class=""></i>
             </div>
           </div>
-          <div class="ml-12 sm:ml-24">
-            <label for="password" class="">Password</label>
+          <div class="w-full sm:w-8/12">
+            <label for="password" class="block ml-12 sm:ml-0">Password</label>
             <div class="relative">
-              <input
-                :type="showPassword ? 'text' : 'password'"
-                id="password"
-                v-model="input.password"
-                class="p-2 rounded-lg m-auto my-4 w-80 shadow-lg shadow-[#a9d2fa]"
-              />
-              <i @click="togglePasswordVisibility"></i>
+              <input :type="showPassword ? 'text' : 'password'" id="password" v-model="input.password" class="p-2 rounded-lg my-4 w-full shadow-lg shadow-[#a9d2fa]" />
+              <i @click="togglePasswordVisibility" class="cursor-pointer"></i>
             </div>
-            <a href="#" class="">Forgot password?</a>
+            <a href="#" class="block ml-12 sm:ml-0">Forgot password?</a>
           </div>
-          <div class=" ml-8 sm:ml-24">
-            <input
-              type="checkbox"
-              v-model="condiciones"
-              id="condiciones"
-              name="condiciones"
-              class=""
-            />
-            <label for="condiciones" class="ml-3" 
-              >I acknowledge and agree to comply with <br/>  thestated policies and 
-              terms</label
-            >
+          <div class="w-full sm:w-8/12 mt-4">
+            <div class="flex items-start ml-12 sm:ml-0">
+              <input type="checkbox" v-model="condiciones" id="condiciones" name="condiciones" />
+              <label for="condiciones" class="ml-3">I acknowledge and agree to comply with <br /> the stated policies and terms</label>
+            </div>
           </div>
-          <div class="ml-8 sm:ml-24">
-            <input type="checkbox" name="remember" class="mr-3" />
-            <label for="remember" class="mr-2">Remember me</label>
+          <div class="w-full sm:w-8/12 mt-4">
+            <div class="flex items-center ml-12 sm:ml-0">
+              <input type="checkbox" name="remember" class="mr-3" />
+              <label for="remember">Remember me</label>
+            </div>
           </div>
-          <button
-            type="submit"
-            @click.prevent="login"
-            :disabled="isButtonDisabled"
-            class="bg-[#127dd6] w-96 h-10 rounded-xl ml-5 mt-5 cursor-pointer sm:mb-12 sm:ml-24 sm:w-80"
-          >
-            Start
-          </button>
+          <button type="submit" @click.prevent="login" :disabled="isButtonDisabled" class="bg-[#127dd6] w-full h-10 rounded-xl mt-5 cursor-pointer sm:mb-12">Start</button>
         </form>
       </div>
     </div>
